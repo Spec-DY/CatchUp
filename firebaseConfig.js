e9e1db61d2d0861ff0211a4a6d870687b3d9b8c0
Spec-DY <ibonehp@gmail.com>
2024-10-14 02:10:25 -0700

    fix firebase auth persistence

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,22 @@
 //DO NOT change firebase to @react-native-firebase, it won't work if we want to use expo
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth';
-
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // This is copied from firebase settings
 // 项目设置->常规，下拉后有个web应用
 const firebaseConfig = {
-  apiKey: "AIzaSyAB6gIGRhUu2jxAEAKfMkEv7kSb5kmafWM",
-  authDomain: "locationsharing-f84da.firebaseapp.com",
-  projectId: "locationsharing-f84da",
-  storageBucket: "locationsharing-f84da.appspot.com",
-  messagingSenderId: "821103618230",
-  appId: "1:821103618230:web:683bbe90368f655c449e17"
+
+  apiKey: "AIzaSyDjppgF_Q8SgrySNKuJTSQITFn2uVL7LuQ",
+  authDomain: "locationsharingapp-51ef6.firebaseapp.com",
+  projectId: "locationsharingapp-51ef6",
+  storageBucket: "locationsharingapp-51ef6.appspot.com",
+  messagingSenderId: "365552357811",
+  appId: "1:365552357811:web:b1e2701522190dea75bbc6"
+
 };
 
-
 export const FIREBASE_APP = initializeApp(firebaseConfig)
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
