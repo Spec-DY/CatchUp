7d4bb3720a3a027336d22d67fc039138f98617d9
ansonhex <ansonhe1997@gmail.com>
2024-11-10 23:26:20 -0800

    update for storage

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,21 +1,24 @@
 //DO NOT change firebase to @react-native-firebase if we want to use expo
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // This is copied from firebase settings
 const firebaseConfig = {
-
-  apiKey: "AIzaSyDjppgF_Q8SgrySNKuJTSQITFn2uVL7LuQ",
-  authDomain: "locationsharingapp-51ef6.firebaseapp.com",
-  projectId: "locationsharingapp-51ef6",
-  storageBucket: "locationsharingapp-51ef6.appspot.com",
-  messagingSenderId: "365552357811",
-  appId: "1:365552357811:web:b1e2701522190dea75bbc6"
-
+  apiKey: process.env.EXPO_PUBLIC_apiKey,
+  authDomain: process.env.EXPO_PUBLIC_authDomain,
+  projectId: process.env.EXPO_PUBLIC_projectId,
+  storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_appId,
 };
 
-export const FIREBASE_APP = initializeApp(firebaseConfig)
+export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
-  persistence: getReactNativePersistence(AsyncStorage)
+  persistence: getReactNativePersistence(AsyncStorage),
 });
+
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
